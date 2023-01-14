82ab83ca6fbabd7170999b2ef27ea8ab99ed2ab1
thinktan <tantao0675@hnu.edu.cn>
2023-01-14 17:42:06 +0800

    bagging

---
+++ 07EnsembleLearningAndRandomForests/BaggingAndPasting.py
@@ -0,0 +1,45 @@
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import make_moons
+
+X, y = make_moons(n_samples=500, noise=0.30, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+
+print(X.shape)
+print(y.shape)
+
+
+# random forests
+from sklearn.ensemble import BaggingClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+bag_clf = BaggingClassifier(
+    DecisionTreeClassifier(), n_estimators=500,
+    max_samples=100, bootstrap=True, random_state=42)
+
+bag_clf.fit(X_train, y_train)
+
+y_pred = bag_clf.predict(X_test)
+
+from sklearn.metrics import accuracy_score
+print("bagging method：%f" % (accuracy_score(y_test, y_pred)))
+
+
+# single classifier
+tree_clf = DecisionTreeClassifier(random_state=42)
+tree_clf.fit(X_train, y_train)
+y_pred_tree = tree_clf.predict(X_test)
+print("single classifier" % (accuracy_score(y_test, y_pred_tree)))
+
+import matplotlib.pyplot as plt
+from Util07 import plot_decision_boundary, save_fig
+
+fig, axes = plt.subplots(ncols=2, figsize=(10,4), sharey=True)
+plt.sca(axes[0])
+plot_decision_boundary(tree_clf, X, y)
+plt.title("Decision Tree", fontsize=14)
+plt.sca(axes[1])
+plot_decision_boundary(bag_clf, X, y)
+plt.title("Decision Trees with Bagging", fontsize=14)
+plt.ylabel("")
+save_fig("decision_tree_without_and_with_bagging_plot")
+plt.show()
--- 07EnsembleLearningAndRandomForests/Util07.py
+++ 07EnsembleLearningAndRandomForests/Util07.py
@@ -42,3 +42,22 @@ def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
     if tight_layout:
         plt.tight_layout()
     plt.savefig(path, format=fig_extension, dpi=resolution)
+
+
+from matplotlib.colors import ListedColormap
+
+def plot_decision_boundary(clf, X, y, axes=[-1.5, 2.45, -1, 1.5], alpha=0.5, contour=True):
+    x1s = np.linspace(axes[0], axes[1], 100)
+    x2s = np.linspace(axes[2], axes[3], 100)
+    x1, x2 = np.meshgrid(x1s, x2s)
+    X_new = np.c_[x1.ravel(), x2.ravel()]
+    y_pred = clf.predict(X_new).reshape(x1.shape)
+    custom_cmap = ListedColormap(['#fafab0','#9898ff','#a0faa0'])
+    plt.contourf(x1, x2, y_pred, alpha=0.3, cmap=custom_cmap)
+    if contour:
+        custom_cmap2 = ListedColormap(['#7d7d58','#4c4c7f','#507d50'])
+        plt.contour(x1, x2, y_pred, cmap=custom_cmap2, alpha=0.8)
+    plt.plot(X[:, 0][y==0], X[:, 1][y==0], "yo", alpha=alpha)
+    plt.plot(X[:, 0][y==1], X[:, 1][y==1], "bs", alpha=alpha)
+    plt.axis(axes)
+    plt.xlabel(r"$x_1$", fontsize=18)
