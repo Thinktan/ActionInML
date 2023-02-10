679744aa50703f4b14b3c426d273a9dcf68267eb
Thinktan <tantao0675@hnu.edu.cn>
2023-02-10 20:45:25 +0800

    go

--- 09UnSupervisedLearning/PcaKmeansFaceCluster.py
+++ 09UnSupervisedLearning/PcaKmeansFaceCluster.py
@@ -2,3 +2,14 @@
 # learn from
 # https://www.kaggle.com/code/annatu/pca-kmeans-face-cluster/data
 
+import matplotlib.pyplot as plt
+import numpy as np
+from skimage.io import imshow
+from sklearn.decomposition import PCA
+import warnings
+warnings.filterwarnings("ignore")
+
+# load dataset.
+faces_image = np.load('./input/olivetti_faces.npy')
+
+print(faces_image.shape)
