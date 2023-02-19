3332ca530240616ba993c3b040403b0dd7b1c856
Thinktan <tantao0675@hnu.edu.cn>
2023-02-19 21:32:53 +0800

    go

---
+++ 10NeuralNetsWithKeras/BuildingAnImageClassifier.py
@@ -0,0 +1,64 @@
+import tensorflow as tf
+from tensorflow import keras
+import matplotlib.pyplot as plt
+from Util10 import save_fig
+
+print(tf.__version__)
+
+fashion_mnist = keras.datasets.fashion_mnist
+(X_train_full, y_train_full), (X_test, y_test) = fashion_mnist.load_data()
+
+print(X_train_full.shape)
+print(X_test.shape)
+
+# 从6w个训练中取5k个作为验证集
+X_valid, X_train = X_train_full[:5000] / 255., X_train_full[5000:] / 255.
+y_valid, y_train = y_train_full[:5000], y_train_full[5000:]
+X_test = X_test / 255.
+
+# print(X_train[0])
+
+# plt.imshow(X_train[0], cmap="binary")
+# plt.imshow(X_train_full[0], cmap="binary")
+# plt.axis('off')
+# plt.show()
+
+# print(y_train)
+
+class_names = ["T-shirt/top", "Trouser", "Pullover", "Dress", "Coat", "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"]
+# print(class_names[y_train[0]])
+
+# 输出case
+# n_rows = 4
+# n_cols = 10
+# plt.figure(figsize=(n_cols * 1.2, n_rows * 1.2))
+# for row in range(n_rows):
+#     for col in range(n_cols):
+#         index = n_cols * row + col
+#         plt.subplot(n_rows, n_cols, index + 1)
+#         plt.imshow(X_train[index], cmap="binary", interpolation="nearest")
+#         plt.axis('off')
+#         plt.title(class_names[y_train[index]], fontsize=12)
+# plt.subplots_adjust(wspace=0.2, hspace=0.5)
+# save_fig('fashion_mnist_plot', tight_layout=False)
+# plt.show()
+
+model = keras.models.Sequential([
+    keras.layers.Flatten(input_shape=[28, 28]),
+    keras.layers.Dense(300, activation="relu"),
+    keras.layers.Dense(100, activation="relu"),
+    keras.layers.Dense(10, activation="softmax")
+])
+
+print(model.summary())
+
+# keras.utils.plot_model(model, "my_fashion_mnist_model.png", show_shapes=True)
+
+model.compile(loss="sparse_categorical_crossentropy",
+              optimizer="sgd",
+              metrics=["accuracy"])
+
+model.compile(loss="sparse_categorical_crossentropy",
+              optimizer="sgd",
+              metrics=["accuracy"])
+
